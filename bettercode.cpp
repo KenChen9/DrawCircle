9ed2383ddb296fde3dff2900ba82c08db2398313
Ken Chen <ken27281@gmail.com>
2021-02-07 00:39:50 +0800

    better

---
+++ bettercode.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int pth(int x, int y) {
+    return sqrt(pow(x, 2) + pow(y, 2));
+}
+
+int main() {
+
+    int r = 10;
+
+    const int length = r;
+    const int width = r;
+
+    for (int y = length; y >= -length; y--) {
+        for (int x = -width; x <= width; x++) {
+
+            if (pth(x, y) >= r) cout << "**";
+            else cout << "  ";
+
+        }
+        cout << "\n";
+    }
+    cin.get();
+
+    return 0;
+}
